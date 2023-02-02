86dac9a00ac3ae219ffc43fa3922c6dfba57c40e
Jungkwon Lee <jklee@superb-ai.com>
2023-02-02 14:44:07 +0900

    add built files

---
+++ build/precache-manifest.845862beb66b485159115457f1f96210.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcec21642abd10d649947939f9d221c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb4fea21fc69905776cc",
+    "url": "/static/css/2.b6f51f8b.chunk.css"
+  },
+  {
+    "revision": "ac134ccb4725cae172cc",
+    "url": "/static/css/main.2f1ce725.chunk.css"
+  },
+  {
+    "revision": "cb4fea21fc69905776cc",
+    "url": "/static/js/2.3c3e95dd.chunk.js"
+  },
+  {
+    "revision": "c08c799342d51af1e236cce853650c4c",
+    "url": "/static/js/2.3c3e95dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac134ccb4725cae172cc",
+    "url": "/static/js/main.a19c73ea.chunk.js"
+  },
+  {
+    "revision": "02a1c20f2daf46a96a47",
+    "url": "/static/js/runtime-main.295d5b3a.js"
+  }
+]);
